69bea439a23d3e0377d880f09cdaefd487f3dc7d
nelbot <nelson.cuadras@gmail.com>
2015-05-11 15:27:59 -0400

    style login, signup, dashboard and campaigns

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,3 +1,43 @@
+function openSignupModal (event) {
+  console.debug(event.target);
+  event.preventDefault();
+  $('.js-signup-modal').modal();
+
+}
+
+$('.js-open-signup-modal').on('click', openSignupModal);
+
+
+function openLoginModal (event) {
+  console.debug(event.target);
+  event.preventDefault();
+  $('.js-login-modal').modal();
+
+}
+
+$('.js-open-login-modal').on('click', openLoginModal);
+
+
+function openLoginBrandModal (event) {
+  console.debug(event.target);
+  event.preventDefault();
+  $('.js-login-brand-modal').modal();
+
+}
+
+$('.js-open-login-brand-modal').on('click', openLoginBrandModal);
+
+
+function openLoginInfluencerModal (event) {
+  console.debug(event.target);
+  event.preventDefault();
+  $('.js-login-influencer-modal').modal();
+
+}
+
+$('.js-open-login-influencer-modal').on('click', openLoginInfluencerModal);
+
+
 function openBrandModal (event) {
   console.debug(event.target);
   event.preventDefault();
